78d4345496d20dcc665ec79cdbef8cf1b3605ee6
vonmoraes <lucas.moraes.989785@sga.pucminas.br>
2019-03-27 23:28:54 -0300

    Adicionando projeto de java do netbeans

---
+++ src/leitor_codigo_barras/Leitor_Codigo_Barras.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leitor_codigo_barras;
+
+import view.Tela_inicial;
+
+/**
+ *
+ * @author moraes
+ */
+public class Leitor_Codigo_Barras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // INICIALIZAR TELA
+        new Tela_inicial().setVisible(true);
+    }
+    
+}
